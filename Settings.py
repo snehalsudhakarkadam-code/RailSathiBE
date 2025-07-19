5b738614f5dc587e28945286b635f597d1f7e00b
snehalsudhakarkadam-code <snehalsudhakarkadam@gmail.com>
2025-07-19 13:41:20 +0530

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
+    }
+}
