dd9ae5e490ae5479cdec3b53cb490361801a8346
snehalsudhakarkadam-code <snehalsudhakarkadam@gmail.com>
2025-07-19 13:23:20 +0530

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -1,1 +1,11 @@
 settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_postgres_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
